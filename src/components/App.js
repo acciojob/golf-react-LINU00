68f83817a4da6a54825ec1ee2605666361ae5348
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-11-06 00:20:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
@@ -11,23 +11,43 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+		 this.handleKeyDown = this.handleKeyDown.bind(this);
     };
 
     buttonClickHandler() {
-   
+		this.setState({renderBall : true})
    }
+
+	handleKeyDown(event){
+		if(event.keyCode === 39){
+		
+			this.setState((prevState) => {
+				const newPos = prevState.posi + 5;
+				return {
+					posi : newPos,
+					 ballPosition: { left: newPos + "px" }
+				}
+			})
+	   }
+	}
+
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
 
-    // bind ArrowRight keydown event
+  
     componentDidMount() {
-      
+      document.addEventListener("keydown",this.handleKeyDown)
     }
+
+ componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
 
     render() {
         return (
